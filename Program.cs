6b098ea0c6e6ee6cb1ef7f9e677e480534e9ca01
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-06-15 20:04:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть число A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть число B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Висновок:");
+
+        for (int i = A; i <= B; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
